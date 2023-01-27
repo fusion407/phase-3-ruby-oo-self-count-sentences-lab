4ba59a94c77c34fc354d7010acce307a0a0ae351
fusion407 <fusion407@gmail.com>
2023-01-26 19:40:48 -0600

    commit tree 47c9683060307e0af2b8d692cb18ecfdc3af1c62

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    count=0
+    split_sentence = self.split
+    split_sentence.each do |letter|
+      if(letter.sentence? or letter.question? or letter.exclamation? )
+        count += 1
+      end
+    end
+    return count
   end
 end
